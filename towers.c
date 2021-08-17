d663b74e1681a8e58cacd85ca3eef9f5b58b29a8
Andrew <martyn.jandrew@gmail.com>
2021-08-17 12:59:53 -0400

    Comments/Refactor

--- towers.c
+++ towers.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Structs
 typedef struct Towers {
     int N;
     int numPegs;
@@ -14,16 +15,16 @@ typedef struct Peg {
 } Peg;
 
 
+// Function Prototypes
+Towers * createTowers(int N);
+void move(char src, char dest);
 void solve(int a, char src, char aux, char dest);
 void show();
-
-Towers * createTowers(int N);
-Peg * createPeg(char name, int N);
-void move(char src, char dest);
 
 Towers * towers;
 
 int main(int argc, char * argv[]) {
+    // Check for arguments
     if(argc == 2) {
         int N = atoi(argv[1]);
         towers = createTowers(N);
@@ -31,49 +32,13 @@ int main(int argc, char * argv[]) {
         solve(N, 'A', 'B', 'C');
         show();
     } else {
-        printf("Usage: ./towers <NumberOfDiscs>\n");
+        printf("Usage: towers.exe <NumberOfDiscs>\n");
     }
 
     return 0;
 }
 
-void solve(int a, char src, char aux, char dest) {
-    if(a == 1) {
-        move(src, dest);
-        printf("Moved disc %d from %c to %c\n", a, src, dest);
-        return;
-    } else {
-        solve(a-1, src, dest, aux);
-        move(src, dest);
-        printf("Moved disc %d from %c to %c\n", a, src, dest);
-        solve(a-1, aux, src, dest);
-    }
-}
-
-void show() {
-    char str1[10], str2[10], str3[10];
-    for(int i = towers->N-1; i >= 0; i--) {
-        towers->pegs[0].discs[i] != -1 ? itoa(towers->pegs[0].discs[i], str1, 10) : strcpy(str1, "");
-        towers->pegs[1].discs[i] != -1 ? itoa(towers->pegs[1].discs[i], str2, 10) : strcpy(str2, "");
-        towers->pegs[2].discs[i] != -1 ? itoa(towers->pegs[2].discs[i], str3, 10) : strcpy(str3, "");
-
-        printf("%s\t%s\t%s\n", str1, str2, str3);
-    }
-    printf("%c\t%c\t%c\n", towers->pegs[0].name, towers->pegs[1].name, towers->pegs[2].name);
-}
-
-// void show() {
-//     for(int i = 0; i < towers->numPegs; i++) {
-//         printf("%c: ", towers->pegs[i].name);
-//         for(int j = 0; j < towers->pegs[i].numDiscs; j++) {
-//             printf("%d ", towers->pegs[i].discs[j]);
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-    
-// }
-
+// Allocates and returns a Towers struct with an assumed numPegs of 3
 Towers * createTowers(int N) {
     Towers * ret = (Towers*)malloc(sizeof(Towers));
     ret->N = N;
@@ -98,23 +63,40 @@ Towers * createTowers(int N) {
     return ret;
 }
 
-Peg * createPeg(char name, int N) {
-    Peg * ret = malloc(sizeof(Peg));
-    ret->name = name;
-    ret->discs = malloc(N*sizeof(int));
-    for(int i = 0; i < N; i++) 
-        ret->discs[i] = -1;
-    ret->numDiscs = 0;
-}
-
+// Move a disc from one peg to another and update structs
 void move(char src, char dest) {
-    if(towers->pegs[src-65].numDiscs < 1) {
-        printf("no");
+    if(towers->pegs[src-65].numDiscs < 1) 
         return;
-    }
 
     towers->pegs[dest-65].discs[towers->pegs[dest-65].numDiscs] = towers->pegs[src-65].discs[towers->pegs[src-65].numDiscs-1];
     towers->pegs[src-65].discs[towers->pegs[src-65].numDiscs-1] = -1;
     towers->pegs[src-65].numDiscs--;
     towers->pegs[dest-65].numDiscs++;
+}
+
+// Standard solve function
+void solve(int a, char src, char aux, char dest) {
+    if(a == 1) {
+        move(src, dest);
+        printf("Moved disc %d from %c to %c\n", a, src, dest);
+        return;
+    } else {
+        solve(a-1, src, dest, aux);
+        move(src, dest);
+        printf("Moved disc %d from %c to %c\n", a, src, dest);
+        solve(a-1, aux, src, dest);
+    }
+}
+
+// Print the towers in a pretty ish way.
+void show() {
+    char str1[10], str2[10], str3[10];
+    for(int i = towers->N-1; i >= 0; i--) {
+        towers->pegs[0].discs[i] != -1 ? itoa(towers->pegs[0].discs[i], str1, 10) : strcpy(str1, "");
+        towers->pegs[1].discs[i] != -1 ? itoa(towers->pegs[1].discs[i], str2, 10) : strcpy(str2, "");
+        towers->pegs[2].discs[i] != -1 ? itoa(towers->pegs[2].discs[i], str3, 10) : strcpy(str3, "");
+
+        printf("%s\t%s\t%s\n", str1, str2, str3);
+    }
+    printf("%c\t%c\t%c\n", towers->pegs[0].name, towers->pegs[1].name, towers->pegs[2].name);
 }
